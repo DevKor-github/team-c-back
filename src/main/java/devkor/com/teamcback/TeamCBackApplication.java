e814a2b1f1e443150c3c3b2bbaa13b667d6a444e
leeyejin <leeyejin113@gmail.com>
2024-03-20 22:56:44 +0900

    Initial Commit

---
+++ src/main/java/devkor/com/teamcback/TeamCBackApplication.java
@@ -0,0 +1,13 @@
+package devkor.com.teamcback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamCBackApplication.class, args);
+    }
+
+}

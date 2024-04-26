c7e6bdc66d6a42e4516beaf29cd64a5a35c1fd3c
leeyejin <leeyejin113@gmail.com>
2024-04-26 20:27:27 +0900

    Feat: UserDetailsImpl 추가

---
+++ src/main/java/devkor/com/teamcback/global/security/UserDetailsImpl.java
@@ -0,0 +1,53 @@
+package devkor.com.teamcback.global.security;
+
+import devkor.com.teamcback.domain.user.entity.Role;
+import devkor.com.teamcback.domain.user.entity.User;
+import java.util.Collection;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Role role = user.getRole();
+        return List.of(role::getAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

4e56766dc355351fabcd2a8c60b4f16a7d37e4a6
leeyejin <leeyejin113@gmail.com>
2024-04-26 20:25:47 +0900

    Feat: UserRepository 추가

---
+++ src/main/java/devkor/com/teamcback/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package devkor.com.teamcback.domain.user.repository;
+
+import devkor.com.teamcback.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

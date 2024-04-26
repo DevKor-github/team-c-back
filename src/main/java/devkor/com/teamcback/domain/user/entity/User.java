dc9acee41950bf16d659c78f2a78d52f8a104c20
leeyejin <leeyejin113@gmail.com>
2024-04-26 20:24:18 +0900

    Feat: User entity 추가

---
+++ src/main/java/devkor/com/teamcback/domain/user/entity/User.java
@@ -0,0 +1,28 @@
+package devkor.com.teamcback.domain.user.entity;
+
+import devkor.com.teamcback.domain.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "tb_user")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String profileUrl;
+
+    @Column(nullable = false)
+    private Role role;
+}

15ef7efb1ec2de002a9cdd61c71f3c9acc37fc03
leeyejin <leeyejin113@gmail.com>
2024-04-28 13:45:12 +0900

    Feat: JwtAuthorizationFilter 추가

---
+++ src/main/java/devkor/com/teamcback/global/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,93 @@
+package devkor.com.teamcback.global.jwt;
+
+import static devkor.com.teamcback.global.jwt.JwtUtil.ACCESS_TOKEN_HEADER;
+import static devkor.com.teamcback.global.response.ResultCode.*;
+
+import devkor.com.teamcback.global.exception.GlobalException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j(topic = "JWT validation & authorization")
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+
+        String accessToken = jwtUtil.getAccessTokenFromCookie(request);
+        log.info("Access Token: {}", accessToken);
+
+        // access token 비어있으면 인증 미처리
+        if (!StringUtils.hasText(accessToken)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // access token 검증
+        switch(jwtUtil.validateToken(accessToken)) {
+            case VALID -> setAuthentication(jwtUtil.getUserIdFromToken(accessToken));
+            case INVALID -> throw new GlobalException(UNAUTHORIZED);
+            case EXPIRED -> authenticateRefreshToken(request, response);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * 인증 처리 (인증 객체를 생성하여 context에 설정)
+     */
+    private void setAuthentication(String userId) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    /**
+     * Refresh token 검증 (access token이 만료된 경우)
+     */
+    private void authenticateRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = jwtUtil.getRefreshTokenFromCookie(request);
+        log.info("Refresh Token: {}", refreshToken);
+
+        if(refreshToken == null) throw new GlobalException(REFRESH_TOKEN_REQUIRED); // refresh token 요청
+
+        switch(jwtUtil.validateToken(refreshToken)) {
+            case VALID -> renewAccessToken(response, refreshToken);
+            case INVALID ->  throw new GlobalException(UNAUTHORIZED); // Unauthorized
+            case EXPIRED -> throw new GlobalException(LOG_IN_REQUIRED); // 재로그인 요청
+        }
+    }
+
+    /**
+     * Access token 재발급 후 요청 처리
+     */
+    private void renewAccessToken(HttpServletResponse response, String refreshToken) {
+        String userId = jwtUtil.getUserIdFromToken(refreshToken);
+        String role = jwtUtil.getRoleFromToken(refreshToken);
+
+        String accessToken = jwtUtil.createAccessToken(userId, role);
+        response.addHeader(ACCESS_TOKEN_HEADER, accessToken);
+
+        setAuthentication(userId);
+    }
+}

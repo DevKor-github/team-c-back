36e800aaebfe4f1aa5f6a36763b9c188170c081a
leeyejin <leeyejin113@gmail.com>
2024-05-09 13:53:18 +0900

    Chore: TODO 기록

--- src/main/java/devkor/com/teamcback/domain/search/service/SearchService.java
+++ src/main/java/devkor/com/teamcback/domain/search/service/SearchService.java
@@ -14,7 +14,6 @@ import devkor.com.teamcback.domain.search.dto.response.GetSearchLogRes;
 import devkor.com.teamcback.domain.search.dto.response.GlobalSearchRes;
 import devkor.com.teamcback.domain.search.entity.PlaceType;
 import devkor.com.teamcback.domain.search.entity.SearchLog;
-import devkor.com.teamcback.domain.user.entity.User;
 import devkor.com.teamcback.domain.user.repository.UserRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -30,7 +29,6 @@ public class SearchService {
     private final BuildingRepository buildingRepository;
     private final BuildingNicknameRepository buildingNicknameRepository;
     private final ClassroomNicknameRepository classroomNicknameRepository;
-    private final UserRepository userRepository;
     private final FacilityRepository facilityRepository;
     private final RedisTemplate<String, SearchLog> searchLogRedis;
 
@@ -116,6 +114,7 @@ public class SearchService {
 
         Long size = searchLogRedis.opsForList().size(key);
 
+        // TODO: 중복되는 검색 기록 제거
         if(size != null && size.equals(10L)) {
             searchLogRedis.opsForList().rightPop(key);
         }

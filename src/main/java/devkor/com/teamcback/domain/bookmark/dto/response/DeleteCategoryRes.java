2bfe86e2f9aab4d98c871eab43b3da6134e3758e
leeyejin <leeyejin113@gmail.com>
2024-07-04 19:33:35 +0900

    [#11] Feat: 카테고리 생성, 삭제

---
+++ src/main/java/devkor/com/teamcback/domain/bookmark/dto/response/DeleteCategoryRes.java
@@ -0,0 +1,10 @@
+package devkor.com.teamcback.domain.bookmark.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "카테고리 삭제 완료")
+@JsonIgnoreProperties
+public class DeleteCategoryRes {
+
+}
---
+++ src/main/java/devkor/com/teamcback/domain/bookmark/service/CategoryService.java
@@ -0,0 +1,68 @@
+package devkor.com.teamcback.domain.bookmark.service;
+
+import static devkor.com.teamcback.global.response.ResultCode.NOT_FOUND_CATEGORY;
+import static devkor.com.teamcback.global.response.ResultCode.NOT_FOUND_USER;
+import static devkor.com.teamcback.global.response.ResultCode.UNAUTHORIZED;
+
+import devkor.com.teamcback.domain.bookmark.dto.request.CreateCategoryReq;
+import devkor.com.teamcback.domain.bookmark.dto.response.CreateCategoryRes;
+import devkor.com.teamcback.domain.bookmark.dto.response.DeleteCategoryRes;
+import devkor.com.teamcback.domain.bookmark.entity.Category;
+import devkor.com.teamcback.domain.bookmark.repository.CategoryRepository;
+import devkor.com.teamcback.domain.user.entity.User;
+import devkor.com.teamcback.domain.user.repository.UserRepository;
+import devkor.com.teamcback.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 카테고리 생성
+     */
+    @Transactional
+    public CreateCategoryRes createCategory(Long userId, CreateCategoryReq req) {
+        User user = findUser(userId);
+
+        categoryRepository.save(new Category(req, user));
+
+        return new CreateCategoryRes();
+    }
+
+    /**
+     * 카테고리 삭제
+     */
+    @Transactional
+    public DeleteCategoryRes deleteCategory(Long userId, Long categoryId) {
+        User user = findUser(userId);
+        Category category = findCategory(categoryId);
+
+        // 카테고리 소유자인지 확인
+        checkAuthority(user, category.getUser());
+
+        categoryRepository.delete(category);
+
+        return new DeleteCategoryRes();
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new GlobalException(NOT_FOUND_USER));
+    }
+
+    private Category findCategory(Long categoryId) {
+        return categoryRepository.findById(categoryId).orElseThrow(() -> new GlobalException(NOT_FOUND_CATEGORY));
+    }
+
+    private void checkAuthority(User accessor, User owner) {
+        if(!accessor.getUserId().equals(owner.getUserId())) {
+            throw new GlobalException(UNAUTHORIZED);
+        }
+    }
+}

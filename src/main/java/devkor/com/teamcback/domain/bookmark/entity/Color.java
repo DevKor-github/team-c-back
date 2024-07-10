1eb7354e0e0301a722d64d8243ba53044d46f75c
leeyejin <leeyejin113@gmail.com>
2024-07-10 15:13:15 +0900

    [#15] Fix: 카테고리 색상 코드 삭제

--- src/main/java/devkor/com/teamcback/domain/bookmark/entity/Color.java
+++ src/main/java/devkor/com/teamcback/domain/bookmark/entity/Color.java
@@ -10,17 +10,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Color {
-    RED("red", "#ED1C24"),
-    ORANGE("orange", "#FF7F27"),
-    YELLOW("yellow", "#FFF200"),
-    GREEN("green", "#22B14C"),
-    BLUE("blue", "#276DDB"),
-    PURPLE("purple", "#984EE8"),
-    PINK("pink", "#FF64B7"),
-    BROWN("brown", "#C9642D"),
-    BLACK("black", "#000000");
+    RED("red"),
+    ORANGE("orange"),
+    YELLOW("yellow"),
+    GREEN("green"),
+    BLUE("blue"),
+    PURPLE("purple"),
+    PINK("pink"),
+    BROWN("brown"),
+    BLACK("black");
     private final String name;
-    private final String code;
 
     // Enum Validation 을 위한 코드, enum 에 속하지 않으면 예외처리
     @JsonCreator

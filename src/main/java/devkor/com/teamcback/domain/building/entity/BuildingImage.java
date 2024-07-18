bf76f21ed46b51c04f6314b771ecfced1fa9d218
leeyejin <leeyejin113@gmail.com>
2024-07-18 17:13:11 +0900

    [#20]Feat: 내부 지도를 위한 BuildingImage 추가

---
+++ src/main/java/devkor/com/teamcback/domain/building/entity/BuildingImage.java
@@ -0,0 +1,31 @@
+package devkor.com.teamcback.domain.building.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tb_building_image")
+@NoArgsConstructor
+public class BuildingImage {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private Double floor;
+
+    @Column(nullable = false)
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "building_id")
+    private Building building;
+}

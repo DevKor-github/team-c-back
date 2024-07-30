683819573b07b73697d6b71487eb81ed09a1feb7
Yeoeun Yang <yyang78077@naver.com>
2024-07-31 03:41:44 +0900

    swagger 추가

--- src/main/java/devkor/com/teamcback/domain/search/dto/response/GetMainFacilityRes.java
+++ src/main/java/devkor/com/teamcback/domain/search/dto/response/GetMainFacilityRes.java
@@ -2,13 +2,18 @@ package devkor.com.teamcback.domain.search.dto.response;
 
 import devkor.com.teamcback.domain.facility.entity.Facility;
 import devkor.com.teamcback.domain.facility.entity.FacilityType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class GetMainFacilityRes {
+    @Schema(description = "편의시설명", example = "2층 학생식당")
     private String name;
+    @Schema(description = "편의시설 종류", example = "CAFETERIA")
     private FacilityType type;
+    @Schema(description = "편의시설 id", example = "1")
     private Long placeId;
+    @Schema(description = "이미지 URL")
     private String imageUrl;
 
     public GetMainFacilityRes(Facility facility) {
--- src/main/java/devkor/com/teamcback/domain/search/dto/response/searchPlaceByMaskIndexRes.java
+++ src/main/java/devkor/com/teamcback/domain/search/dto/response/searchPlaceByMaskIndexRes.java
@@ -3,14 +3,18 @@ package devkor.com.teamcback.domain.search.dto.response;
 import devkor.com.teamcback.domain.bookmark.entity.PlaceType;
 import devkor.com.teamcback.domain.classroom.entity.Classroom;
 import devkor.com.teamcback.domain.facility.entity.Facility;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "maskIndex 대응 장소 정보")
 public class searchPlaceByMaskIndexRes {
-    private Long placeId;
+    @Schema(description = "장소 종류", example = "CLASSROOM")
     private PlaceType placeType;
+    @Schema(description = "장소 Id", example = "1")
+    private Long placeId;
 
     public searchPlaceByMaskIndexRes(Classroom classroom) {
         this.placeType = PlaceType.CLASSROOM;

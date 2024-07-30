13d15e33ed5a9c883f5271a02d0d8854abe31e4f
Yeoeun Yang <yyang78077@naver.com>
2024-07-31 03:41:44 +0900

    swagger 추가

--- src/main/java/devkor/com/teamcback/domain/search/dto/response/SearchBuildingDetailRes.java
+++ src/main/java/devkor/com/teamcback/domain/search/dto/response/SearchBuildingDetailRes.java
@@ -1,19 +1,26 @@
 package devkor.com.teamcback.domain.search.dto.response;
 
 import devkor.com.teamcback.domain.building.entity.Building;
-import devkor.com.teamcback.domain.facility.entity.Facility;
 import devkor.com.teamcback.domain.facility.entity.FacilityType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@Schema(description = "건물 상세 정보")
 public class SearchBuildingDetailRes {
+    @Schema(description = "건물명", example = "애기능생활관")
     private String name;
+    @Schema(description = "주소", example = "서울 성북구 안암로 73-15")
     private String address;
+    @Schema(description = "건물 정보(TMI)", example = "애기능생활관이다.")
     private String details;
+    @Schema(description = "북마크 저장 여부", example = "false")
     private boolean bookmarked;
+    @Schema(description = "건물 내 시설 종류", example = "LOUNGE, GYM, ...")
     private List<FacilityType> existTypes; //건물 내 facility 종류 리스트(아이콘용)
+    @Schema(description = "주요 시설 리스트")
     private List<GetMainFacilityRes> mainFacilityList;
 
     public SearchBuildingDetailRes(List<GetMainFacilityRes> facilities, List<FacilityType> types, Building building, boolean bookmarked) {

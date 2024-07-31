60d79eee8430b21a5c5dc70786c9657a8358fec3
Yeoeun Yang <yyang78077@naver.com>
2024-08-01 04:33:54 +0900

    야외 관련 예외 처리

--- src/main/java/devkor/com/teamcback/domain/search/dto/response/SearchPlaceDetailRes.java
+++ src/main/java/devkor/com/teamcback/domain/search/dto/response/SearchPlaceDetailRes.java
@@ -31,17 +31,22 @@ public class SearchPlaceDetailRes {
     private boolean bookmarked;
     private boolean isOperating;
     private String nextPlaceTime;
+
     public SearchPlaceDetailRes(Classroom classroom, boolean bookmarked) {
         this.type = PlaceType.CLASSROOM;
         this.id = classroom.getId();
-        this.name = classroom.getName();
+        if(classroom.getBuilding().getId() == 0) {
+            this.name = classroom.getName();
+            this.longitude = classroom.getNode().getLongitude();
+            this.latitude = classroom.getNode().getLatitude();
+        } else {
+            this.name = classroom.getBuilding().getName() + " " + classroom.getName();
+            this.xCoord = classroom.getNode().getXCoord();
+            this.yCoord = classroom.getNode().getYCoord();
+        }
         this.detail = classroom.getDetail();
         this.plugAvailability = classroom.isPlugAvailability();
         this.imageUrl = classroom.getImageUrl();
-        this.longitude = classroom.getNode().getLongitude();
-        this.latitude = classroom.getNode().getLatitude();
-        this.xCoord = classroom.getNode().getXCoord();
-        this.yCoord = classroom.getNode().getYCoord();
         this.maskIndex = classroom.getMaskIndex();
         this.bookmarked = bookmarked;
         //TODO: 나중에 운영 정보 수정해서 넣기
@@ -53,16 +58,20 @@ public class SearchPlaceDetailRes {
         this.type = PlaceType.FACILITY;
         this.id = facility.getId();
         this.facilityType = facility.getType();
-        this.name = facility.getName();
+        if(facility.getBuilding().getId() == 0) {
+            this.name = facility.getName();
+            this.longitude = facility.getNode().getLongitude();
+            this.latitude = facility.getNode().getLatitude();
+        } else {
+            this.name = facility.getBuilding().getName() + " " + facility.getName();
+            this.xCoord = facility.getNode().getXCoord();
+            this.yCoord = facility.getNode().getYCoord();
+        }
         this.detail = facility.getDetail();
         this.availability = facility.isAvailability();
         this.plugAvailability = facility.isPlugAvailability();
         this.imageUrl = facility.getImageUrl();
         this.operatingTime = facility.getOperatingTime();
-        this.longitude = facility.getNode().getLongitude();
-        this.latitude = facility.getNode().getLatitude();
-        this.xCoord = facility.getNode().getXCoord();
-        this.yCoord = facility.getNode().getYCoord();
         this.maskIndex = facility.getMaskIndex();
         this.bookmarked = bookmarked;
         //TODO: 나중에 운영 정보 수정해서 넣기

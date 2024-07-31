3d09f7e312e17b5dc18f426a429306ec4d87e743
leeyejin <leeyejin113@gmail.com>
2024-07-31 15:48:02 +0900

    [#28]Feat: 공휴일 판단 메서드 추가

--- src/main/java/devkor/com/teamcback/domain/operatingtime/repositoy/HolidayRepository.java
+++ src/main/java/devkor/com/teamcback/domain/operatingtime/repositoy/HolidayRepository.java
@@ -1,8 +1,10 @@
 package devkor.com.teamcback.domain.operatingtime.repositoy;
 
 import devkor.com.teamcback.domain.operatingtime.entity.Holiday;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HolidayRepository extends JpaRepository<Holiday, Long> {
 
+    boolean existsByDateAndIsHoliday(LocalDate date, boolean isHoliday);
 }

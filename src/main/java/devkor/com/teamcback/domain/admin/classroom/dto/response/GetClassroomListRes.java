07c637d745a42cab65f7aefefc0fc8a358ee8e5b
Yeoeun Yang <yyang78077@naver.com>
2024-08-13 13:43:25 +0900

    Classroom Admin 기능 추가

---
+++ src/main/java/devkor/com/teamcback/domain/admin/classroom/dto/response/GetClassroomListRes.java
@@ -0,0 +1,17 @@
+package devkor.com.teamcback.domain.
+    admin.classroom.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.util.List;
+
+@Schema(description = "강의실 List 조회 응답 dto")
+@Getter
+public class GetClassroomListRes {
+    private List<GetClassroomRes> classroomList;
+
+    public GetClassroomListRes(List<GetClassroomRes> classroomList) {
+        this.classroomList = classroomList;
+    }
+}

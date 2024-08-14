2556616b7465b3ecd1dffe5d4226a7d07a15a3c8
leeyejin <leeyejin113@gmail.com>
2024-08-14 16:49:59 +0900

    Fix: id 생성 전략 수정

--- src/main/java/devkor/com/teamcback/domain/operatingtime/entity/OperatingCondition.java
+++ src/main/java/devkor/com/teamcback/domain/operatingtime/entity/OperatingCondition.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OperatingCondition extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)

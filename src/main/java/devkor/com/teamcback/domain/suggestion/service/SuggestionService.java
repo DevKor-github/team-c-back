dc1fd0824ec1b29f15eb30630329103021703978
leeyejin <leeyejin113@gmail.com>
2024-08-17 17:02:25 +0900

    [#42]Feat: 건의 조회

--- src/main/java/devkor/com/teamcback/domain/suggestion/service/SuggestionService.java
+++ src/main/java/devkor/com/teamcback/domain/suggestion/service/SuggestionService.java
@@ -1,11 +1,17 @@
 package devkor.com.teamcback.domain.suggestion.service;
 
-import devkor.com.teamcback.domain.bookmark.dto.request.CreateCategoryReq;
 import devkor.com.teamcback.domain.suggestion.dto.request.CreateSuggestionReq;
 import devkor.com.teamcback.domain.suggestion.dto.response.CreateSuggestionRes;
+import devkor.com.teamcback.domain.suggestion.dto.response.GetSuggestionRes;
 import devkor.com.teamcback.domain.suggestion.entity.Suggestion;
+import devkor.com.teamcback.domain.suggestion.entity.SuggestionType;
 import devkor.com.teamcback.domain.suggestion.repository.SuggestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,5 +30,28 @@ public class SuggestionService {
         Suggestion savedSuggestion = suggestionRepository.save(suggestion);
 
         return new CreateSuggestionRes(savedSuggestion);
+    }
+
+    /**
+     * 건의 조회
+     */
+    @Transactional(readOnly = true)
+    public Page<GetSuggestionRes> getSuggestions(int page, int size, String sortBy, boolean isAsc, SuggestionType type, Boolean isSolved) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        if (type == null && isSolved == null) {
+            return suggestionRepository.findAll(pageable).map(GetSuggestionRes::new);
+        }
+        else if (type == null) {
+            return suggestionRepository.findByIsSolved(pageable, isSolved).map(GetSuggestionRes::new);
+        }
+        else if (isSolved == null) {
+            return suggestionRepository.findBySuggestionType(pageable, type).map(GetSuggestionRes::new);
+        }
+        else {
+            return suggestionRepository.findBySuggestionTypeAndIsSolved(pageable, type, isSolved).map(GetSuggestionRes::new);
+        }
     }
 }

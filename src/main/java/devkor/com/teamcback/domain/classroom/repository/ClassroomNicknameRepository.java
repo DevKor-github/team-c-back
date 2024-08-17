e6c8948261312cdd7962a882c25eedaa1938bcdf
Yeoeun Yang <yyang78077@naver.com>
2024-08-17 17:30:50 +0900

    통합 검색 기능 수정

--- src/main/java/devkor/com/teamcback/domain/classroom/repository/ClassroomNicknameRepository.java
+++ src/main/java/devkor/com/teamcback/domain/classroom/repository/ClassroomNicknameRepository.java
@@ -4,12 +4,14 @@ import devkor.com.teamcback.domain.classroom.entity.Classroom;
 import devkor.com.teamcback.domain.classroom.entity.ClassroomNickname;
 import java.util.Collection;
 import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClassroomNicknameRepository extends JpaRepository<ClassroomNickname, Long> {
-    List<ClassroomNickname> findByNicknameContaining(String word);
+    List<ClassroomNickname> findByNicknameContaining(String word, Pageable pageable);
 
     List<ClassroomNickname> findAllByClassroom(Classroom classroom);
 
-    List<ClassroomNickname> findByNicknameContainingAndClassroomIn(String word, List<Classroom> list);
+    List<ClassroomNickname> findByNicknameContainingAndClassroomIn(String word, List<Classroom> list,  Pageable pageable);
 }

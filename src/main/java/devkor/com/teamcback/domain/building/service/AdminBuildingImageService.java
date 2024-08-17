32a0518eec237a21b592af096578bcb10cf48ed2
leeyejin <leeyejin113@gmail.com>
2024-08-17 19:28:51 +0900

    Chore: admin 파일 이동

---
+++ src/main/java/devkor/com/teamcback/domain/building/service/AdminBuildingImageService.java
@@ -0,0 +1,104 @@
+package devkor.com.teamcback.domain.building.service;
+
+import static devkor.com.teamcback.global.response.ResultCode.DUPLICATED_BUILDING_IMAGE;
+import static devkor.com.teamcback.global.response.ResultCode.INCORRECT_FLOOR;
+import static devkor.com.teamcback.global.response.ResultCode.NOT_FOUND_BUILDING;
+import static devkor.com.teamcback.global.response.ResultCode.NOT_FOUND_BUILDING_IMAGE;
+
+import devkor.com.teamcback.domain.building.dto.response.DeleteBuildingImageRes;
+import devkor.com.teamcback.domain.building.dto.response.ModifyBuildingImageRes;
+import devkor.com.teamcback.domain.building.dto.response.SaveBuildingImageRes;
+import devkor.com.teamcback.domain.building.dto.response.SearchBuildingImageRes;
+import devkor.com.teamcback.domain.building.entity.Building;
+import devkor.com.teamcback.domain.building.entity.BuildingImage;
+import devkor.com.teamcback.domain.building.repository.BuildingImageRepository;
+import devkor.com.teamcback.domain.building.repository.BuildingRepository;
+import devkor.com.teamcback.global.exception.GlobalException;
+import devkor.com.teamcback.infra.s3.FilePath;
+import devkor.com.teamcback.infra.s3.S3Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class AdminBuildingImageService {
+    private final BuildingRepository buildingRepository;
+    private final BuildingImageRepository buildingImageRepository;
+    private final S3Util s3Util;
+
+    // 건물 내부 사진 저장
+    @Transactional
+    public SaveBuildingImageRes saveBuildingImage(Long buildingId, Double floor, MultipartFile image) {
+        String imageUrl = s3Util.uploadFile(image, FilePath.BUILDING_IMAGE);
+        Building building = findBuilding(buildingId);
+
+        if(floor < building.getUnderFloor() * (-1) || floor > building.getFloor()) {
+            throw new GlobalException(INCORRECT_FLOOR);
+        }
+        checkExistedImage(building, floor); // 해당 건물, 층에 해당하는 사진이 있는지 확인
+        BuildingImage buildingImage = buildingImageRepository.save(new BuildingImage(floor, imageUrl, building));
+
+        return new SaveBuildingImageRes(buildingImage);
+    }
+
+    // 건물 내부 사진 수정
+    @Transactional
+    public ModifyBuildingImageRes modifyBuildingImage(Long buildingImageId, MultipartFile image) {
+        BuildingImage buildingImage = findBuildingImage(buildingImageId);
+        if(s3Util.exists(buildingImage.getImage(), FilePath.BUILDING_IMAGE)) {
+            s3Util.deleteFile(buildingImage.getImage(), FilePath.BUILDING_IMAGE); // 기존 사진 S3에서 삭제
+        }
+
+        String imageUrl = s3Util.uploadFile(image, FilePath.BUILDING_IMAGE);
+        buildingImage.update(imageUrl);
+
+        return new ModifyBuildingImageRes(buildingImage);
+    }
+
+    // 건물 내부 사진 삭제
+    @Transactional
+    public DeleteBuildingImageRes deleteBuildingImage(Long buildingImageId) {
+        BuildingImage buildingImage = findBuildingImage(buildingImageId);
+        if(s3Util.exists(buildingImage.getImage(), FilePath.BUILDING_IMAGE)) {
+            s3Util.deleteFile(buildingImage.getImage(), FilePath.BUILDING_IMAGE); // 기존 사진 S3에서 삭제
+        }
+        buildingImageRepository.delete(buildingImage);
+
+        return new DeleteBuildingImageRes();
+    }
+
+    // 건물 내부 사진 검색
+    @Transactional(readOnly = true)
+    public SearchBuildingImageRes searchBuildingImage(Long buildingId, Double floor) {
+        Building building = findBuilding(buildingId);
+        BuildingImage buildingImage = findBuildingImage(building, floor);
+
+        return new SearchBuildingImageRes(buildingImage);
+    }
+
+    private Building findBuilding(Long buildingId) {
+        return buildingRepository.findById(buildingId).orElseThrow(() -> new GlobalException(NOT_FOUND_BUILDING));
+    }
+
+    private BuildingImage findBuildingImage(Long buildingImageId) {
+        return buildingImageRepository.findById(buildingImageId).orElseThrow(() -> new GlobalException(NOT_FOUND_BUILDING_IMAGE));
+    }
+
+    private BuildingImage findBuildingImage(Building building, Double floor) {
+        BuildingImage buildingImage = buildingImageRepository.findByBuildingAndFloor(building, floor);
+        if(buildingImage == null) {
+            throw new GlobalException(NOT_FOUND_BUILDING_IMAGE);
+        }
+        return buildingImage;
+
+    }
+
+    private void checkExistedImage(Building building, Double floor) {
+        BuildingImage buildingImage = buildingImageRepository.findByBuildingAndFloor(building, floor);
+        if(buildingImage != null) {
+            throw new GlobalException(DUPLICATED_BUILDING_IMAGE);
+        }
+    }
+}
---
+++ src/main/java/devkor/com/teamcback/domain/classroom/controller/AdminClassroomNicknameController.java
@@ -0,0 +1,75 @@
+package devkor.com.teamcback.domain.classroom.controller;
+
+import devkor.com.teamcback.domain.classroom.dto.request.SaveClassroomNicknameReq;
+import devkor.com.teamcback.domain.classroom.dto.response.DeleteClassroomNicknameRes;
+import devkor.com.teamcback.domain.classroom.dto.response.GetClassroomNicknameListRes;
+import devkor.com.teamcback.domain.classroom.dto.response.SaveClassroomNicknameRes;
+import devkor.com.teamcback.domain.classroom.service.AdminClassroomNicknameService;
+import devkor.com.teamcback.global.response.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/classrooms")
+public class AdminClassroomNicknameController {
+    private final AdminClassroomNicknameService adminClassroomNicknameService;
+
+    @PostMapping("/{classroomId}/nicknames")
+    @Operation(summary = "강의실 별명 저장",
+        description = "강의실 별명 저장")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "정상 처리 되었습니다."),
+        @ApiResponse(responseCode = "404", description = "강의실을 찾을 수 없습니다.",
+            content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+        @ApiResponse(responseCode = "401", description = "권한이 없습니다.",
+            content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+    })
+    public CommonResponse<SaveClassroomNicknameRes> saveClassroomNickname(
+        @Parameter(name = "classroomId", description = "강의실 ID") @PathVariable Long classroomId,
+        @Parameter(description = "강의실 별명 저장 dto") @RequestBody SaveClassroomNicknameReq req) {
+        return CommonResponse.success(adminClassroomNicknameService.saveClassroomNickname(classroomId, req));
+    }
+
+    @DeleteMapping("/nicknames/{nicknameId}")
+    @Operation(summary = "강의실 별명 삭제",
+        description = "강의실 별명 삭제")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "정상 처리 되었습니다."),
+        @ApiResponse(responseCode = "404", description = "강의실 별명을 찾을 수 없습니다.",
+            content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+        @ApiResponse(responseCode = "401", description = "권한이 없습니다.",
+            content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+    })
+    public CommonResponse<DeleteClassroomNicknameRes> deleteClassroomNickname(
+        @Parameter(name = "nicknameId", description = "강의실 별명 ID") @PathVariable Long nicknameId) {
+        return CommonResponse.success(adminClassroomNicknameService.deleteClassroomNickname(nicknameId));
+    }
+
+    @GetMapping("/{classroomId}/nicknames")
+    @Operation(summary = "강의실 별명 조회",
+        description = "강의실 별명 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "정상 처리 되었습니다."),
+        @ApiResponse(responseCode = "404", description = "강의실을 찾을 수 없습니다.",
+            content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+        @ApiResponse(responseCode = "401", description = "권한이 없습니다.",
+            content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+    })
+    public CommonResponse<GetClassroomNicknameListRes> getClassroomNickname(
+        @Parameter(name = "classroomId", description = "강의실 ID") @PathVariable Long classroomId) {
+        return CommonResponse.success(adminClassroomNicknameService.getClassroomNickname(classroomId));
+    }
+}

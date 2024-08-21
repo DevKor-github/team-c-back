1e16c5d04d2a696b1b1ce115a49f678f5c9455e4
leeyejin <leeyejin113@gmail.com>
2024-08-21 15:59:12 +0900

    Fix: 편의시설이 있는 건물 조회를 건물 전체 조회 api와 통합, 특정 편의시설 목록 조회 API 추가

---
+++ src/main/java/devkor/com/teamcback/domain/search/dto/response/SearchBuildingFacilityListRes.java
@@ -0,0 +1,30 @@
+package devkor.com.teamcback.domain.search.dto.response;
+
+import devkor.com.teamcback.domain.building.entity.Building;
+import devkor.com.teamcback.domain.facility.entity.FacilityType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+
+@Schema(description = "특정 건물의 편의시설 조회 결과")
+@Getter
+public class SearchBuildingFacilityListRes {
+    @Schema(description = "편의시설이 위치한 건물 ID", example = "1")
+    private Long buildingId;
+    @Schema(description = "편의시설이 위치한 건물 이름", example = "애기능생활관")
+    private String buildingName;
+    @Schema(description = "편의시설 종류", example = "TRASH_CAN")
+    private FacilityType type;
+    @Schema(description = "각 층별 편의시설 조회 결과")
+    @Setter
+    private Map<Double, List<SearchFacilityRes>> facilities = new HashMap<>();
+
+    public SearchBuildingFacilityListRes(Building building, FacilityType type) {
+        this.buildingId = building.getId();
+        this.buildingName = building.getName();
+        this.type = type;
+    }
+}

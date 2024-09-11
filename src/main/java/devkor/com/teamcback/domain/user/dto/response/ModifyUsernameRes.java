bc8f285da44bd330ff3199f1a1512da09b16070a
Yeoeun Yang <yyang78077@naver.com>
2024-09-11 17:08:37 +0900

    스웨거 오타 수정

--- src/main/java/devkor/com/teamcback/domain/user/dto/response/ModifyUsernameRes.java
+++ src/main/java/devkor/com/teamcback/domain/user/dto/response/ModifyUsernameRes.java
@@ -3,7 +3,7 @@ package devkor.com.teamcback.domain.user.dto.response;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "유저명 수정 완료")
+@Schema(description = "사용자명 수정 완료")
 @JsonIgnoreProperties
 public class ModifyUsernameRes {
 }

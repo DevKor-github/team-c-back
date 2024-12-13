72786df4996befd56d6a50219ec94d35eaa19401
leeyejin <leeyejin113@gmail.com>
2024-12-13 18:20:54 +0900

    Refactor: AdminException 추가

---
+++ src/main/java/devkor/com/teamcback/global/exception/AdminException.java
@@ -0,0 +1,14 @@
+package devkor.com.teamcback.global.exception;
+
+import devkor.com.teamcback.global.response.ResultCode;
+import lombok.Getter;
+
+@Getter
+public class AdminException extends GlobalException{
+    private String adminMessage;
+
+    public AdminException(ResultCode resultCode, String adminMessage) {
+        super(resultCode);
+        this.adminMessage = adminMessage;
+    }
+}

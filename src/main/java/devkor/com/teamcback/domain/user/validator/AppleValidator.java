45631a3010c56aa931643d6ef612516a5f87afa2
leeyejin <leeyejin113@gmail.com>
2025-01-18 14:39:50 +0900

    Fix: 예외 처리 오류 수정

--- src/main/java/devkor/com/teamcback/domain/user/validator/AppleValidator.java
+++ src/main/java/devkor/com/teamcback/domain/user/validator/AppleValidator.java
@@ -50,6 +50,8 @@ public class AppleValidator {
             OIDCDecodePayload payload = oidcUtil.getOIDCTokenBody(token, oidcPublicKeyDto.getN(), oidcPublicKeyDto.getE());
 
             return payload.getSub();
+        } catch(GlobalException e) {
+            throw new GlobalException(LOG_IN_REQUIRED);
         } catch (Exception e) {
             throw new GlobalException(INVALID_TOKEN);
         }

13901c7639421db843cf88a0ac66113f76bf0066
leeyejin <leeyejin113@gmail.com>
2025-03-12 15:01:09 +0900

    Feat: SuggestionImage 클래스 추가

---
+++ src/main/java/devkor/com/teamcback/domain/suggestion/entity/SuggestionImage.java
@@ -0,0 +1,23 @@
+package devkor.com.teamcback.domain.suggestion.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tb_suggestion_image")
+@NoArgsConstructor
+public class SuggestionImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "suggestionId", nullable = false)
+    private Suggestion suggestion;
+
+}

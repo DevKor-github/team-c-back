3ce7be80266a5f5e5aea2db9429faa1736e2b980
Yeoeun Yang <yyang78077@naver.com>
2025-03-17 15:29:54 +0900

    Fix: Test Disabled

--- src/test/java/devkor/com/teamcback/global/aop/UpdateScoreAspectTest.java
+++ src/test/java/devkor/com/teamcback/global/aop/UpdateScoreAspectTest.java
@@ -26,6 +26,7 @@ import java.util.List;
 import static devkor.com.teamcback.global.response.ResultCode.NOT_FOUND_USER;
 
 @Slf4j
+@Disabled
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UpdateScoreAspectTest {

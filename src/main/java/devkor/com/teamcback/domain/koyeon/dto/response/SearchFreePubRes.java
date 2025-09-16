4b75f0f86d7e3eda01ef6a4ad93aa26dbe143049
Yeoeun Yang <yyang78077@naver.com>
2025-09-16 21:47:54 +0900

    fix: DTO 내 주소/메뉴 추가

--- src/main/java/devkor/com/teamcback/domain/koyeon/dto/response/SearchFreePubRes.java
+++ src/main/java/devkor/com/teamcback/domain/koyeon/dto/response/SearchFreePubRes.java
@@ -2,10 +2,9 @@ package devkor.com.teamcback.domain.koyeon.dto.response;
 
 import devkor.com.teamcback.domain.koyeon.entity.FreePub;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Schema(description = "주점 정보")
 @Getter
@@ -24,10 +23,12 @@ public class SearchFreePubRes {
     private Double latitude;
     @Schema(description = "노드 ID", example = "1")
     private Long nodeId = null;
+    @Schema(description = "주점 주소", example = "서울특별시 성북구 5가")
+    private String address;
     @Schema(description = "태그에 해당하는 음식 리스트", example = "[\"떡볶이\"]")
-    private List<String> filteredMenus = new ArrayList<>();
+    private List<String> menus;
 
-    public SearchFreePubRes(FreePub pub) {
+    public SearchFreePubRes(FreePub pub, List<String> menus) {
         this.id = pub.getId();
         this.name = pub.getName();
         this.sponsor = pub.getSponsor();
@@ -35,16 +36,7 @@ public class SearchFreePubRes {
         this.latitude = pub.getLatitude();
         this.longitude = pub.getLongitude();
         if(pub.getNode() != null) this.nodeId = pub.getNode().getId();
-    }
-
-    public SearchFreePubRes(FreePub pub, List<String> filteredMenus) {
-        this.id = pub.getId();
-        this.name = pub.getName();
-        this.sponsor = pub.getSponsor();
-        this.operatingTime = pub.getOperatingTime();
-        this.latitude = pub.getLatitude();
-        this.longitude = pub.getLongitude();
-        if(pub.getNode() != null) this.nodeId = pub.getNode().getId();
-        this.filteredMenus = filteredMenus;
+        this.address = pub.getAddress();
+        this.menus = menus;
     }
 }

9fb5bd5b72a13e5f4135ab058d777cc229fb2747
yejin <leeyejin113@gmail.com>
2025-11-16 13:16:54 +0900

    Feat: 애플 서버 알림 개발 중

--- src/main/java/devkor/com/teamcback/domain/user/controller/AppleController.java
+++ src/main/java/devkor/com/teamcback/domain/user/controller/AppleController.java
@@ -1,6 +1,8 @@
 package devkor.com.teamcback.domain.user.controller;
 
+import com.apple.itunes.storekit.verification.VerificationException;
 import devkor.com.teamcback.domain.user.dto.request.AppleNotationReq;
+import devkor.com.teamcback.domain.user.dto.response.AppleNotificationRes;
 import devkor.com.teamcback.domain.user.service.AppleService;
 import devkor.com.teamcback.global.exception.exception.GlobalException;
 import devkor.com.teamcback.global.response.CommonResponse;
@@ -22,23 +24,9 @@ public class AppleController {
      * Apple 로그인 계정 상태 알림을 수신하는 엔드포인트
      */
     @PostMapping("/notifications")
-    public CommonResponse<> handleAppleNotification(@RequestBody AppleNotationReq request) {
+    public CommonResponse<AppleNotificationRes> handleAppleNotification(@RequestBody AppleNotationReq request) throws VerificationException {
 
-        String signedPayload = request.getSignedPayload();
+        return CommonResponse.success(appleService.handleAppleNotification(request));
 
-        if (signedPayload == null || signedPayload.isEmpty()) {
-            return new GlobalException(ResultCode.INVALID_INPUT);
-        }
-
-        try {
-            // 1. JWS 검증 및 디코딩 후 비즈니스 로직 수행
-            notificationService.processSignedPayload(signedPayload);
-
-            // 2. Apple 서버에 성공적인 수신을 알리기 위해 반드시 200 OK를 반환해야 합니다.
-            return new CommonResponse.success();
-        } catch (Exception e) {
-            System.err.println("Apple SiWA Notification processing failed: " + e.getMessage());
-            return new GlobalException(INTERNAL_SERVER_ERROR);
-        }
     }
 }

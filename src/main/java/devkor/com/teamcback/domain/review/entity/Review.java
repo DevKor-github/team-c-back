f0518ce005c4e5c1a657a270436161794da625eb
danbaram0420 <68544417+danbaram0420@users.noreply.github.com>
2026-02-25 19:07:14 +0900

    Update Review.java

--- src/main/java/devkor/com/teamcback/domain/review/entity/Review.java
+++ src/main/java/devkor/com/teamcback/domain/review/entity/Review.java
@@ -36,6 +36,7 @@ public class Review extends BaseEntity {
     @Column(nullable = false, length = 500)
     private String comment = "";     // 500자까지 작성 가능
 
+    @Setter
     @Column(nullable = false)
     private boolean isReported = false;  // 신고 여부
 

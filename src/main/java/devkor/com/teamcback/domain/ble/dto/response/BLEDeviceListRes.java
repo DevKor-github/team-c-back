be778b03c2263ba40c13a9c119fd7b01bead3bd8
danbaram0420 <danbaram0420@korea.ac.kr>
2026-04-15 10:33:13 +0900

    ble/list 기능 추가

--- src/main/java/devkor/com/teamcback/domain/ble/dto/response/BLEDeviceListRes.java
+++ src/main/java/devkor/com/teamcback/domain/ble/dto/response/BLEDeviceListRes.java
@@ -1,8 +1,9 @@
 package devkor.com.teamcback.domain.ble.dto.response;
 
-import devkor.com.teamcback.domain.place.entity.Place;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,12 +14,19 @@ public class BLEDeviceListRes {
     private Long placeId;
     private Integer capacity;
     private String imageUrl;
+    private Integer lastCount;
+    private Integer lastStatus;
+    private LocalDateTime lastTime;
 
-    public BLEDeviceListRes(Long id, String deviceName, Long placeId, int capacity, String imageUrl) {
+    public BLEDeviceListRes(Long id, String deviceName, Long placeId, int capacity, String imageUrl,
+                            Integer lastCount, Integer lastStatus, LocalDateTime lastTime) {
         this.id = id;
         this.deviceName = deviceName;
         this.placeId = placeId;
         this.capacity = capacity;
         this.imageUrl = imageUrl;
+        this.lastCount = lastCount;
+        this.lastStatus = lastStatus;
+        this.lastTime = lastTime;
     }
 }

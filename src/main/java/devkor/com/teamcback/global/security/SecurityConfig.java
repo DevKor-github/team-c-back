fa6bb9a6a27c1a050d111242389b6f8c776f5a4d
Jokebear777 <korae9955@naver.com>
2026-07-31 20:33:59 +0900

    docs: restore SecurityConfig comments

--- src/main/java/devkor/com/teamcback/global/security/SecurityConfig.java
+++ src/main/java/devkor/com/teamcback/global/security/SecurityConfig.java
@@ -81,22 +81,22 @@ public class SecurityConfig {
             sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
-        http.authorizeHttpRequests(authorizeHttpRequests ->
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers(HttpMethod.POST, "/api/search/**").authenticated()
-                        .requestMatchers("/api/users/login/**").permitAll()
+                        .requestMatchers("/api/users/login/**").permitAll() // 로그인은 허용
                         .requestMatchers("/api/users/**").authenticated()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN") // 관리자인 경우에만 허용
                         .requestMatchers("/api/categories/**").authenticated()
                         .requestMatchers("/api/bookmarks/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/api/reviews/**").authenticated()
-                        .requestMatchers("/api/reports/status").authenticated()
-                        .requestMatchers("/api/notifications/installations/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/reviews/**").authenticated() // 리뷰는 로그인 필요
+                        .requestMatchers("/api/reports/status").authenticated() // 신고 상태 확인은 로그인 필요
+                        .requestMatchers("/api/notifications/installations/**").authenticated() // 토큰 등록 로그인 필요
                         .anyRequest().permitAll()
         ).exceptionHandling(ex -> ex
-                .accessDeniedHandler(customAccessDeniedHandler())
-                .authenticationEntryPoint(customAuthenticationEntryPoint())
+                .accessDeniedHandler(customAccessDeniedHandler()) // 인가 실패 시
+                .authenticationEntryPoint(customAuthenticationEntryPoint()) // 인증 실패 시
         );
 
         http.logout(
